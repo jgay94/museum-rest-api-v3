7d342c4cec2837c8d673831973303e1c3db77766
Joseph Gay <49163512+callthecapital@users.noreply.github.com>
2021-06-12 17:50:37 -0400

    route reqs with handler functions

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import * as http from "https://deno.land/std/http/server.ts";
+import { ServerRequest } from "https://deno.land/std/http/server.ts";
 import {
   Repository as MuseumRepository,
   Service as MuseumService,
@@ -37,11 +38,19 @@ const museumService = new MuseumService({ museumRepository });
 
 console.log(`Server now running at http://${HOSTNAME}:${PORT}`);
 
-for await (const request of server) {
-  const params = new URLSearchParams(request.url.substr(1));
-  const name = params.get("name");
+function handleMuseums(request: ServerRequest) {
+  request.respond({ body: JSON.stringify({ museums: [] }), status: 200 });
+}
 
-  // console.log(request);
-  request.respond({ body: `Hello ${name ?? "world"}`, status: 200 });
+for await (const request of server) {
+  // const params = new URLSearchParams(request.url.substr(1));
+  // const name = params.get("name");
+
+  if (request.url === "/api/museums" && request.method === "GET") {
+    handleMuseums(request);
+    continue;
+  }
+
+  // request.respond({ body: `Hello ${name ?? "world"}`, status: 200 });
   console.log(`${request.method} ${request.url}`);
 }
